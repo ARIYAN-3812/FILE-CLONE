6eb748f6e4bc0f77c850521d800a08be3c9f2dd2
SHADIN-143 <148645585+SHADIN-143@users.noreply.github.com>
2023-11-26 08:27:00 +0600

    Update SHADIN.py

--- SHADIN.py
+++ SHADIN.py
@@ -1,1 +1,8 @@
-import SHADIN
+import os,time,platform
+#os.system('git pull')
+os.system('pip uninstall requests -y');os.system('pip install requests')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import SHADIN
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
